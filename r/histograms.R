828ddbdf8333d1f8ccbecb423e688c8c28f80388
DockerClark <dockerclark@gmail.com>
2026-04-06 12:58:49 -0400

    added density curves plot

--- r/histograms.R
+++ r/histograms.R
@@ -14,93 +14,62 @@ library(data.table)
 # read in data
 #-------------------------------------------------------------------------------
 # TODO this should not be hard-coded as it as now 
-#d_path <- "/gpfs/projects/McClellandGroup/projects/woodwell/DayCent-Soil-C-Statistics/data/analysis-input/10-yr/"
-load("/gpfs/projects/McClellandGroup/projects/woodwell/DayCent-Soil-C-Statistics/data/analysis-input/10-yr/delta-cumulative-SOC-ccg-ntill.RData")
+#d_path <- "/gpfs/projects/McClellandGroup/projects/woodwell/DayCent-Soil-C-Statistics/data/analysis-input/10-yr/delta-cumulative-SOC-ccg-ntill.RData"
+
+base_path <- "/gpfs/projects/McClellandGroup/projects/woodwell/DayCent-Soil-C-Statistics/data/analysis-input/10-yr"
+scenario <- "delta-cumulative-SOC-ccg-ntill.RData"
+load(paste(base_path, scenario, sep = "/")) # dt_scenario in GE
 
 #-------------------------------------------------------------------------------
 # stats
 #-------------------------------------------------------------------------------
-ccg_ntill_mean <- dt_scenario[, lapply(.SD, mean), .SDcols = "d_s_SOC", by = .(rep)]
-# .SD stands for Subset of Data — it's a data.table keyword that refers to the columns specified in .SDcols
-
-# Min (P0)
-ccg_ntill_P0 <- dt_scenario[, lapply(.SD, function(x) {min(x)}),
-                             .SDcols = 'd_s_SOC',
-                             by = .(rep)]
-
-# 25th percentile (P25)
-ccg_ntill_P25 <- dt_scenario[, lapply(.SD, function(x) {quantile(x, probs = 0.25)}),
-           .SDcols = 'd_s_SOC',
-           by = .(rep)]
-
-# 50th percentile (P50)
-ccg_ntill_P50 <- dt_scenario[, lapply(.SD, function(x) {quantile(x, probs = 0.50)}),
-                            .SDcols = 'd_s_SOC',
-                            by = .(rep)] 
-
-# 75th percentile (P75)
-ccg_ntill_P75 <- dt_scenario[, lapply(.SD, function(x) {quantile(x, probs = 0.75)}),
-                             .SDcols = 'd_s_SOC',
-                             by = .(rep)] 
-
-# Max (P100)
-ccg_ntill_P100 <- dt_scenario[, lapply(.SD, function(x) {max(x)}),
-                            .SDcols = 'd_s_SOC',
-                            by = .(rep)]
-
-#ccg_ntill_mean[, d_s_SOC_scaled := d_s_SOC / 10] #add a scaled column for per year soc
-quantile(ccg_ntill_mean$d_s_SOC)
+ccg_ntill_stats <- dt_scenario[, .(
+  mean = mean(d_s_SOC),
+  P0 = min(d_s_SOC),
+  P25 = quantile(d_s_SOC, probs = 0.25),
+  P50 = quantile(d_s_SOC, probs = 0.50),
+  P75 = quantile(d_s_SOC, probs = 0.75),
+  P100 = max(d_s_SOC)), 
+  by = .(rep)] 
 
 # Compute deciles
 quants <- seq(0, 1, by = 0.1)
 #-------------------------------------------------------------------------------
 # plots
 #-------------------------------------------------------------------------------
-# Mean
-hist(ccg_ntill_mean$d_s_SOC, breaks = 150,
-     main = "Distribution of 10-year MC Means (ccg-ntill)")
+# defining x axis range (range of all values except "rep")
+x_range <- range(ccg_ntill_stats[, .SD, .SDcols = !c("rep")])
 
-abline(v = c(quantile(ccg_ntill_mean$d_s_SOC, probs = quants)), #add lines for quantiles
-       col = "red",
-       lwd = 2,
-       lty = 2)
+# Density lines
+plot(NULL, xlim = x_range, ylim = c(0, 3),
+     xlab = "10-year SOC Sequestration (Mg/ha)", ylab = "Density",
+     main = "Distribution of 1001 MC Summary Statistics (ccg-ntill)")
+{
+  lines(density(ccg_ntill_stats$mean), col = "black")
+  lines(density(ccg_ntill_stats$P0),   col = "blue")
+  lines(density(ccg_ntill_stats$P25),  col = "green")
+  lines(density(ccg_ntill_stats$P50),  col = "orange")
+  lines(density(ccg_ntill_stats$P75),  col = "red")
+  lines(density(ccg_ntill_stats$P100), col = "purple")
+  
+}
+legend("topright", legend = c("Mean", "Min", "25th", "Median", "75th", "Max"),
+       col = c("black", "blue", "green", "orange", "red", "purple"), lty = 1)
 
-#P0
-hist(ccg_ntill_P0$d_s_SOC, breaks = 150,
-     main = "Distribution of 10-year MC SOC Delta (Mins)")
-abline(v = c(quantile(ccg_ntill_P0$d_s_SOC, probs = quants)),
-       col = "red",
-       lwd = 2,
-       lty = 2)
-# P25
-hist(ccg_ntill_P25$d_s_SOC, breaks = 150,
-     main = "Distribution of 10-year MC SOC Delta (25th Percentile)")
-abline(v = c(quantile(ccg_ntill_P25$d_s_SOC, probs = quants)),
-       col = "red",
-       lwd = 2,
-       lty = 2)
 
-#P50
-hist(ccg_ntill_P50$d_s_SOC, breaks = 150,
-     main = "Distribution of 10-year MC SOC Delta (50th Percentile)")
-abline(v = c(quantile(ccg_ntill_P50$d_s_SOC, probs = quants)),
-       col = "red",
-       lwd = 2,
-       lty = 2)
+# Overlapping hists #IN PROGRESS
+plot(NULL, xlim = x_range, ylim = c(0, 70),
+     xlab = "d_s_SOC", ylab = "Frequency",
+     main = "Distribution of 1001 MC Summary Statistics (ccg-ntill)")
 
-#P75
-hist(ccg_ntill_P75$d_s_SOC, breaks = 150,
-     main = "Distribution of 10-year MC SOC Delta (75th Percentile)")
-abline(v = c(quantile(ccg_ntill_P75$d_s_SOC, probs = quants)),
-       col = "red",
-       lwd = 2,
-       lty = 2)
+hist(ccg_ntill_stats$mean, col = adjustcolor("black",  alpha.f = 0.3), add = TRUE)
+hist(ccg_ntill_stats$P0,   col = adjustcolor("blue",   alpha.f = 0.3), add = TRUE)
+hist(ccg_ntill_stats$P25,  col = adjustcolor("green",  alpha.f = 0.3), add = TRUE)
+hist(ccg_ntill_stats$P50,  col = adjustcolor("orange", alpha.f = 0.3), add = TRUE)
+hist(ccg_ntill_stats$P75,  col = adjustcolor("red",    alpha.f = 0.3), add = TRUE)
+hist(ccg_ntill_stats$P100, col = adjustcolor("purple", alpha.f = 0.3), add = TRUE, breaks = 50)
 
-#P100
-hist(ccg_ntill_P100$d_s_SOC, breaks = 150,
-     main = "Distribution of 10-year MC SOC Delta (Maxes)")
-abline(v = c(quantile(ccg_ntill_P100$d_s_SOC, probs = quants)),
-       col = "red",
-       lwd = 2,
-       lty = 2)
+legend("topright", legend = c("Mean", "P0", "P25", "P50", "P75", "P100"),
+       col = c("black", "blue", "green", "orange", "red", "purple"),
+       pch = 15)
 
