943ee56e355a9d6955fd40a0159e27e6dd99a39c
DockerClark <dockerclark@gmail.com>
2026-04-22 17:23:39 -0400

    test map

--- r/geo-analysis.R
+++ r/geo-analysis.R
@@ -1,6 +1,6 @@
 # filename:     geo-analysis.R    
 # created:      20 April 2026
-# last updated: 20 April 2026
+# last updated: 22 April 2026
 # author:       Docker Clark
 
 # description: 
@@ -11,9 +11,11 @@
 library(data.table)
 library(sf)
 library(terra)
+library(ggplot2)
+library(stringr)
 
 #-------------------------------------------------------------------------------
-# create filepaths
+# directories and startup
 #-------------------------------------------------------------------------------
 #base data path
 b_path <- "/gpfs/projects/McClellandGroup/projects/woodwell/DayCent-Soil-C-Statistics/data"
@@ -28,56 +30,70 @@ args[5] <- "delta-cumulative-SOC"
 #assuming shp_p means shapefile path
 shp_p <- paste(b_path, args[1], "shp", sep = "/")
 
-#add a scenario
+#add a scenario "dt_scenario"
 load(paste0(b_path, "/", args[1], "/",      #base file path
             args[4], "/", args[5], "-",     #time scale & SOC delta
             args[3],".RData"))              #scenario code and extension
+
+#annualize SOC as a new column so either can be used
+yrs <- as.numeric(str_split(args[4], "-")[[1]][1])
+dt_scenario[, an_d_s_SOC := d_s_SOC / yrs]
+
+#for later labeling
+scenario_labels <- c(
+  "conv"      = "Conventional / BAU",
+  "res"       = "Full Residue Retention",
+  "ntill"     = "No-Tillage",
+  "ccg"       = "Grass Cover Crop",
+  "ccl"       = "Legume Cover Crop",
+  "ntill-res" = "No-Tillage & Full Residue Retention",
+  "ccg-res"   = "Grass Cover Crop & Full Residue Retention",
+  "ccl-res"   = "Legume Cover Crop & Full Residue Retention",
+  "ccg-ntill" = "Grass Cover Crop, No-Tillage & Full Residue Retention",
+  "ccl-ntill" = "Legume Cover Crop, No-Tillage & Full Residue Retention"
+)
 #-------------------------------------------------------------------------------
-# Code snippets from SC McClelland
-#-------------------------------------------------------------------------------
-#----------------------------------------------------------------
 # ADD regions
-#----------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # read in shape file #~/analysis-input/shp
 r_shp   <- st_read(paste(shp_p, 'WB_countries_Admin0_10m.shp', sep = '/'))
 # read in crop mask
 r       <- rast(paste(b_path, args[1], 'msw-cropland-rf-ir-area.tif', sep = '/'))
-# keep first layer
+# keep first layer terra::rasterize needs a single layer raster
 r       <- r[[1]]
 
 # create function
 # inputs are the shape file and the raster (above)
-create_WB_cty = function(shp_f, rst) {
+create_WB_cty <- function(shp_f, rst) {
   # ORIGINAL WB NAME and OBJECTID
-  shp_dt          = setDT(as.data.table(st_drop_geometry(shp_f)))
+  shp_dt          <- setDT(as.data.table(st_drop_geometry(shp_f)))
   # MATCH resolution of simulation data, dimensions the same
-  target.r    = rst
-  # CONFIRM SHP in same crs as target
-  country.sf  = st_transform(shp_f, crs(target.r))
-  country_r   = terra::rasterize(
-    x         = vect(country.sf),
-    y         = target.r,
-    field     = "OBJECTID",
-    touches   = TRUE          # optional: include cells touched by polygons
+  target.r    <- rst
+  # CONFIRM SHP in same coord ref syst as target
+  country.sf  <- st_transform(shp_f, crs(target.r))
+  country_r   <- terra::rasterize(
+    x       = vect(country.sf),
+    y       = target.r,
+    field   = "OBJECTID",
+    touches = TRUE          # optional: include cells touched by polygons
   )
   # CREATE data.frame, merge
-  new_shp_dt  = as.data.frame(country_r, cells = TRUE, xy = TRUE)
-  new_shp_dt  = setDT(new_shp_dt) # data.table object
+  new_shp_dt  <- as.data.frame(country_r, cells = TRUE, xy = TRUE)
+  new_shp_dt  <- setDT(new_shp_dt) # data.table object
   # GET WB names to match to ID
-  shp_names   = data.table(WB_NAME   = shp_dt$WB_NAME,
-                           ID        = shp_dt$OBJECTID)
+  shp_names   <- data.table(WB_NAME   = shp_dt$WB_NAME,
+                            ID        = shp_dt$OBJECTID)
   # JOIN with WB names
-  new_shp_dt  = new_shp_dt[shp_names, on = .(OBJECTID = ID)]  
+  new_shp_dt  <- new_shp_dt[shp_names, on = .(OBJECTID = ID)]  
   return(new_shp_dt)
 }
-###### Working through this to understand it #########
-############# stopped here 4/20/206 ##################
+
 # create country data table with function
-WB_dt = create_WB_cty(r_shp, r)
+WB_dt <- create_WB_cty(r_shp, r)
 # join country data table to simulation data
-dt_scenario = dt_scenario[WB_dt[,c('cell', 'WB_NAME')], on = .(gridid = cell)]
+dt_scenario <- dt_scenario[WB_dt[,c('cell', 'WB_NAME', "x", "y")], on = .(gridid = cell)]
 # remove NAs
-dt_scenario = dt_scenario[!is.na(crop)]
+dt_scenario <- dt_scenario[!is.na(crop)]
 setorder(dt_scenario, gridid)
 gc() #garbage collection
 
@@ -99,7 +115,7 @@ AME   = c('Congo, Democratic Republic of', 'Nigeria', 'Tanzania', 'South Africa'
           'Sierra Leone', 'Iraq', 'Rwanda', 'Eritrea', 'eSwatini', 'Benin', 'Burundi', 'Djibouti', 'Equatorial Guinea',
           'Madagascar', 'Mauritania', 'Tunisia', 'Syrian Arab Republic', 'Lebanon', 'Jordan', 'Libya', 'Israel', 
           'West Bank and Gaza', 'Kuwait', 'Oman', 'Qatar', 'United Arab Emirates', 'Yemen, Republic of', 'Cabo Verde',
-          'Guinea-Bissau', 'Togo', 'Comoros', 'Mauritius', 'Lesotho')
+          'Guinea-Bissau', 'Togo', 'Comoros', 'Mauritius', 'Lesotho', "Gambia, The", "Bahrain")
 ADP   = c('China', 'Indonesia', 'India', 'Myanmar', 'Vietnam', 'Malaysia', 'Thailand', 'Pakistan', 'Papua New Guinea',
           'Philippines', 'Bangladesh', 'Cambodia', "Lao People's Democratic Republic", 'Mongolia', 'Korea, Republic of',
           'Afghanistan', 'Nepa', 'Sri Lanka', "Korea, Democratic People's Republic of", 'Solomon Islands', 'Bhutan',
@@ -124,3 +140,31 @@ dt_scenario[WB_NAME %in% EEWCA, IPCC_NAME := 'EEWCA']
 dt_scenario[WB_NAME %in% LAC, IPCC_NAME   := 'LAC']
 
 # check if missing but the groups above should capture everything
+if (nrow(dt_scenario[is.na(IPCC_NAME), .(WB_NAME, IPCC_NAME)]) > 0) {
+  #which countries are not captured
+  missing <- dt_scenario[is.na(IPCC_NAME), unique(WB_NAME)]
+  message("All countries not captured. Missing: ", missing)
+} else {
+  message("All countries captured.")
+}
+
+
+#-------------------------------------------------------------------------------
+# Visualizations
+#-------------------------------------------------------------------------------
+#ok let me start very small and filter this data down to just the US
+#I want to try my hand at a map that just shows d_s_SOC in the USA
+dt_small <- dt_scenario[WB_NAME == "United States of America", ]
+
+ggplot(dt_small, aes(x = x, y = y, fill = d_s_SOC)) +
+  geom_raster() +
+  
+  scale_fill_distiller(palette = "RdYlGn", direction = 1,
+                       name = "SOC Change\n(Mg C/ha)") +
+  coord_sf() +
+  labs(title = paste("Delta Cumulative SOC |", dt_small$WB_NAME[1]),
+       subtitle = paste0("Scenario: ", scenario_labels[args[3]], " | ", args[4]), 
+       x = "Longitude",
+       y = "Latitude") +
+  theme_minimal()
+
